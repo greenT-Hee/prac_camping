680ac70c602c6658ba5d6a3679140017fe8c2c0f
greenT-Hee <heehee970322@gmail.com>
2022-10-11 02:13:04 +0900

    Add:슬라이드 버튼 효과 추가

--- slide-jQuery/index.js
+++ slide-jQuery/index.js
@@ -34,3 +34,61 @@ slidesArr.forEach((item, index) => {
 // }
 
 
+// -----------------------------------------------------
+
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+// 슬라이드 이동 함수
+// .animate : ({css property, duration, easing})
+function goToSlide(index) {
+    // i 0 left:0% i 1 left: -100%
+    slideGroup.animate({left: -100 * index + '%', duration, easing});
+    currentIndex = index; 
+    displayBtns();
+}
+
+// 인디케이터로 이동하기 
+const indicatorItem = document.querySelectorAll('.indicator a');
+
+indicatorItem.forEach((item, index) => {
+    // for(let i = 0; i < item.length; i++) {
+    // }
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        goToSlide(index);
+    })
+})
+
+// 좌우 버튼으로 이동하기 
+// 다음 버튼 클릭 currentIndex + 1 
+// 이전 버튼 클릭 currentIndex -1
+
+nextBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    currentIndex += 1;
+    goToSlide(currentIndex);
+})
+
+prevBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    currentIndex -= 1;
+
+    goToSlide(currentIndex);
+})
+
+function displayBtns() {
+    if( currentIndex === 0) {
+        prevBtn.classList.add('disabled');
+    } else {
+        prevBtn.classList.remove('disabled');
+    }
+
+    if( currentIndex == slideCount - 1) {
+        nextBtn.classList.add('disabled');
+    }else {
+        nextBtn.classList.remove('disabled');
+    }
+}
+displayBtns();
